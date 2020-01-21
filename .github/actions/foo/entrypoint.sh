471b893ef82fd077ed81f7330b1f8601a241419c
Preetam Jinka <preetam@shiftleft.io>
2020-01-21 13:34:11 -0800

    debug

--- .github/actions/foo/entrypoint.sh
+++ .github/actions/foo/entrypoint.sh
@@ -14,8 +14,13 @@ curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/statuses/$GITHUB_SHA" \
   -H "Content-Type: application/json" \
   -d '{"state": "failure", "context": "Security checks", "target_url": "https://example.com"}'
 
+curl "https://api.github.com/repos/$GITHUB_REPO/pulls?base=$GITHUB_BRANCH&state=open" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" | jq '.[0].number'
+
 PULL_REQUEST=$(curl "https://api.github.com/repos/$GITHUB_REPO/pulls?base=$GITHUB_BRANCH&state=open" \
   -H "Authorization: Bearer $GITHUB_TOKEN" | jq '.[0].number')
+
+echo "Got pull request $PULL_REQUEST for branch $GITHUB_BRANCH"
 
 curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/issues/$PULL_REQUEST/comments" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
